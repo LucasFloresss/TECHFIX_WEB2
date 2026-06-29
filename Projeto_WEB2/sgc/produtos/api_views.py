7359e861775b6fed5ed1d07734d2cc6f9461ce32
Lucas Flores <63207885+LucasFloresss@users.noreply.github.com>
2026-06-29 11:18:24 -0300

    Update api_views.py

--- Projeto_WEB2/sgc/produtos/api_views.py
+++ Projeto_WEB2/sgc/produtos/api_views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 from .models import Produto
 from .serializers import ProdutoSerializer
 
+#Cria uma API CRUD completa para o modelo "produto" automaticamente
 class ProdutoViewSet(viewsets.ModelViewSet):
     queryset = Produto.objects.all()
     serializer_class = ProdutoSerializer
