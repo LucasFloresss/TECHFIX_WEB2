41946414e1c13113ed23ccf3cf9907a320b3cd93
Lucas Flores <63207885+LucasFloresss@users.noreply.github.com>
2026-06-29 11:14:36 -0300

    Update serializers.py

--- Projeto_WEB2/sgc/produtos/serializers.py
+++ Projeto_WEB2/sgc/produtos/serializers.py
@@ -4,14 +4,14 @@ from .models import Produto
 class ProdutoSerializer(serializers.ModelSerializer):
     estoque_baixo = serializers.SerializerMethodField()
 
-    class Meta:
+    class Meta: #metadados (bomba)
         model = Produto
         fields = '__all__'
 
     def get_estoque_baixo(self, obj):
-        return obj.quantidade_estoque <= obj.estoque_minimo
+        return obj.quantidade_estoque <= obj.estoque_minimo # estoque_baixo retorna True/False baseado no estoque
 
     def validate_preco(self, value):
-        if value < 0:
+        if value < 0: # Impede preços negativos
             raise serializers.ValidationError('Preço não pode ser negativo.')
         return value
