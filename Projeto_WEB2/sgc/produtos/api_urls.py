373ad8522df3fa70a6aba4b6d2d23d91af97b69b
Lucas Flores <63207885+LucasFloresss@users.noreply.github.com>
2026-06-29 11:19:24 -0300

    Update api_urls.py

--- Projeto_WEB2/sgc/produtos/api_urls.py
+++ Projeto_WEB2/sgc/produtos/api_urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .api_views import ProdutoViewSet
 
+# Gera todas as rotas REST para o ProdutoViewSet automaticamente.
 router = DefaultRouter()
 router.register('produtos', ProdutoViewSet)
 
