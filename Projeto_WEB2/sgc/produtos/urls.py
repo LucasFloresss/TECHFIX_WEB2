6865b10848dedc963efe21e7a0072ab9e6322ac5
Lucas Flores <63207885+LucasFloresss@users.noreply.github.com>
2026-06-29 11:12:35 -0300

    Update urls.py

--- Projeto_WEB2/sgc/produtos/urls.py
+++ Projeto_WEB2/sgc/produtos/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('produtos/', views.lista, name='produtos'),
-    path('produtos/novo/', views.form, name='produto_novo'),
-    path('produtos/<int:pk>/editar/', views.form, name='produto_editar'),
+    path('produtos/', views.lista, name='produtos'), #listar
+    path('produtos/novo/', views.form, name='produto_novo'), #cria
+    path('produtos/<int:pk>/editar/', views.form, name='produto_editar'), #edita kkk
 ]
