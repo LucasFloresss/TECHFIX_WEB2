9546177e8f9bd21900b5b06eee7900277d1729d8
Lucas Flores <63207885+LucasFloresss@users.noreply.github.com>
2026-06-29 09:09:38 -0300

    Update views.py

--- Projeto_WEB2/sgc/clientes/views.py
+++ Projeto_WEB2/sgc/clientes/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
 def lista(request):
-    return render(request, 'clientes/lista.html')
+    return render(request, 'clientes/lista.html') #  lista todos os clientes
 
 def form(request, pk=None):
-    return render(request, 'clientes/form.html', {'cliente_id': pk})
+    return render(request, 'clientes/form.html', {'cliente_id': pk}) # exibe formulário para criar/editar um cliente
+
+ # Controla a lógica de negócio e o fluxo HTTP. Cada função recebe uma requisição e retorna uma resposta.
