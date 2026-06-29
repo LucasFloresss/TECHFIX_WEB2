2b08cdd0d69069b314dd07a8e8584cf2525fac77
Lucas Flores <63207885+LucasFloresss@users.noreply.github.com>
2026-06-29 09:38:39 -0300

    Update models.py

--- Projeto_WEB2/sgc/clientes/models.py
+++ Projeto_WEB2/sgc/clientes/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+# Define o modelo "cliente", que vai se tornar uma tabela no banco de dados com os campos especificados.
 class Cliente(models.Model):
     nome = models.CharField(max_length=200)
     cpf = models.CharField(max_length=14, unique=True)
@@ -7,8 +8,7 @@ class Cliente(models.Model):
     telefone = models.CharField(max_length=20)
     endereco = models.CharField(max_length=300)
 
-    def __str__(self):
+    def __str__(self): # Define que o objeto será texto
         return self.nome
-
     class Meta:
-        ordering = ['nome']
+        ordering = ['nome'] # sempre que buscar clientes, eles virão ordenados alfabeticamente usando o nome
