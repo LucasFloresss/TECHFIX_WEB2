119ca38b00497312760ddc5a55fabdc7270b511c
Lucas Flores <63207885+LucasFloresss@users.noreply.github.com>
2026-06-29 11:07:57 -0300

    Update settings.py

--- Projeto_WEB2/sgc/core/settings.py
+++ Projeto_WEB2/sgc/core/settings.py
@@ -87,7 +87,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(hours=8),
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=8), #tempo pra expirar token de autenticacao
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
 
