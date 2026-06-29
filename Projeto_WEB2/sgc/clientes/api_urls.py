5f39f33b7150bb3384cfdfeca1aacd2eed4c8559
Lucas Flores <63207885+LucasFloresss@users.noreply.github.com>
2026-06-29 09:48:08 -0300

    Update api_urls.py

--- Projeto_WEB2/sgc/clientes/api_urls.py
+++ Projeto_WEB2/sgc/clientes/api_urls.py
@@ -2,7 +2,6 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .api_views import ClienteViewSet
 
-router = DefaultRouter()
-router.register('clientes', ClienteViewSet)
-
-urlpatterns = [path('', include(router.urls))]
+router = DefaultRouter() # roteador automático do DRF que gera todas as URLs REST
+router.register('clientes', ClienteViewSet) # cria automaticamente todas as rotas CRUD
+urlpatterns = [path('', include(router.urls))] # inclui todas as URL geradas pelo router no projeto
