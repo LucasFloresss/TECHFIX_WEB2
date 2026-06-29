0ff54a55537ce3b97d89921e1780bdd346eadcbf
Lucas Flores <63207885+LucasFloresss@users.noreply.github.com>
2026-06-29 09:14:10 -0300

    Update urls.py

--- Projeto_WEB2/sgc/clientes/urls.py
+++ Projeto_WEB2/sgc/clientes/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
+# Mapeia as URLS
 urlpatterns = [
-    path('clientes/', views.lista, name='clientes'),
-    path('clientes/novo/', views.form, name='cliente_novo'),
-    path('clientes/<int:pk>/editar/', views.form, name='cliente_editar'),
+    path('clientes/', views.lista, name='clientes'), # Quando acessar "clientes" ele chama a lista
+    path('clientes/novo/', views.form, name='cliente_novo'), # Chama o formulario
+    path('clientes/<int:pk>/editar/', views.form, name='cliente_editar'), # Chama formulario para editar
 ]
